282a827d804ae14aa781f21e0146d8f9d047b04d
asb1512 <asb1512@gmail.com>
2023-04-27 15:42:18 -0400

    handles interactions with Google Sheets API

---
+++ googleSheet.js
@@ -0,0 +1,34 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+require('dotenv').config();
+
+class InteractWithGoogleSheet {
+  static writeToSheet = async (formData) => {
+    console.log('received data:', formData);
+    const { firstName, lastName, email, zip, mobile, message } = formData;
+    // initialize the Google Sheet
+    const doc = new GoogleSpreadsheet(process.env.GS_SPREADSHEET_ID);
+    // initialize auth to Google Sheet
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GS_CLIENT_EMAIL,
+      private_key: process.env.GS_PRIVATE_KEY,
+    });
+    await doc.loadInfo();
+    // set current sheet
+    const sheet = doc.sheetsByIndex[0];
+
+    // add row
+    await sheet.addRow({
+      firstName,
+      lastName,
+      email,
+      zip,
+      mobile,
+      message,
+    });
+    console.log(sheet);
+
+
+  }
+}
+
+module.exports = InteractWithGoogleSheet;
