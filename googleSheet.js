1b71eac2d17348a236ec1a1d59d0f61cb68b334b
asb1512 <asb1512@gmail.com>
2023-04-28 12:35:03 -0400

    returning status update

--- googleSheet.js
+++ googleSheet.js
@@ -6,27 +6,31 @@ class InteractWithGoogleSheet {
     console.log('received data:', formData);
     const { firstName, lastName, email, zip, mobile, message } = formData;
     // initialize the Google Sheet
-    const doc = new GoogleSpreadsheet(process.env.GS_SPREADSHEET_ID);
-    // initialize auth to Google Sheet
-    await doc.useServiceAccountAuth({
-      client_email: process.env.GS_CLIENT_EMAIL,
-      private_key: process.env.GS_PRIVATE_KEY,
-    });
-    await doc.loadInfo();
-    // set current sheet
-    const sheet = doc.sheetsByIndex[0];
+    try {
+      const doc = new GoogleSpreadsheet(process.env.GS_SPREADSHEET_ID);
+      // initialize auth to Google Sheet
+      await doc.useServiceAccountAuth({
+        client_email: process.env.GS_CLIENT_EMAIL,
+        private_key: process.env.GS_PRIVATE_KEY,
+      });
+      await doc.loadInfo();
+      // set current sheet
+      const sheet = doc.sheetsByIndex[0];
 
-    // add row
-    await sheet.addRow({
-      firstName,
-      lastName,
-      email,
-      zip,
-      mobile,
-      message,
-    });
-    console.log(sheet);
+      // add row
+      await sheet.addRow({
+        firstName,
+        lastName,
+        email,
+        zip,
+        mobile,
+        message,
+      });
 
+      return { status: true, error: null };
+    } catch (e) {
+      throw { status: false, error: e };
+    }
 
   }
 }
