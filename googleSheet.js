246bf56c3cc87689e7cf65175cac626cd1e1be4c
asb1512 <asb1512@gmail.com>
2023-04-28 12:56:48 -0400

    added more console logs for debugging in prod env

--- googleSheet.js
+++ googleSheet.js
@@ -9,33 +9,29 @@ class InteractWithGoogleSheet {
     console.log('client email:', process.env.GS_CLIENT_EMAIL);
     console.log('private key:', process.env.GS_PRIVATE_KEY)
     // initialize the Google Sheet
-    try {
-      const doc = new GoogleSpreadsheet(process.env.GS_SPREADSHEET_ID);
-      console.log('doc:', doc);
-      // initialize auth to Google Sheet
-      await doc.useServiceAccountAuth({
-        client_email: process.env.GS_CLIENT_EMAIL,
-        private_key: process.env.GS_PRIVATE_KEY,
-      });
-      await doc.loadInfo();
-      // set current sheet
-      const sheet = doc.sheetsByIndex[0];
-      console.log('sheet:', sheet);
-      // add row
-      await sheet.addRow({
-        firstName,
-        lastName,
-        email,
-        zip,
-        mobile,
-        message,
-      });
-
-      return { status: true, error: null };
-    } catch (e) {
-      throw { status: false, error: e };
-    }
-
+    const doc = new GoogleSpreadsheet(process.env.GS_SPREADSHEET_ID);
+    console.log('doc:', doc);
+    // initialize auth to Google Sheet
+    const auth = await doc.useServiceAccountAuth({
+      client_email: process.env.GS_CLIENT_EMAIL,
+      private_key: process.env.GS_PRIVATE_KEY,
+    });
+    console.log('auth', auth)
+    const loadedDoc = await doc.loadInfo();
+    console.log('loaded doc:', loadedDoc);
+    // set current sheet
+    const sheet = doc.sheetsByIndex[0];
+    console.log('sheet:', sheet);
+    // add row
+    const addRow = await sheet.addRow({
+      firstName,
+      lastName,
+      email,
+      zip,
+      mobile,
+      message,
+    });
+    console.log('addRow', addRow);
   }
 }
 
