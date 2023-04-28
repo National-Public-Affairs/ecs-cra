a135cf4d0f87a0a7810b64643b2133cf038b9958
asb1512 <asb1512@gmail.com>
2023-04-28 16:09:02 -0400

    added sheet var

--- googleSheet.js
+++ googleSheet.js
@@ -7,13 +7,13 @@ class InteractWithGoogleSheet {
     // initialize the Google Sheet
     const doc = new GoogleSpreadsheet(process.env.GS_SPREADSHEET_ID);
     // initialize auth to Google Sheet
-    await doc.useServiceAccountAuth({
+    const auth = await doc.useServiceAccountAuth({
       client_email: process.env.GS_CLIENT_EMAIL,
       private_key: process.env.GS_PRIVATE_KEY.split(String.raw`\n`).join('\n'),
     });
     await doc.loadInfo();
     // set current sheet
-    doc.sheetsByIndex[0];
+    const sheet = doc.sheetsByIndex[0];
     // add row
     await sheet.addRow({
       firstName,
