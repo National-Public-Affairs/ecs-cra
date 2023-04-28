0af8a62b091345016563a81226b337def51cac19
asb1512 <asb1512@gmail.com>
2023-04-28 12:44:23 -0400

    logging for debug in prod env

--- googleSheet.js
+++ googleSheet.js
@@ -5,9 +5,13 @@ class InteractWithGoogleSheet {
   static writeToSheet = async (formData) => {
     console.log('received data:', formData);
     const { firstName, lastName, email, zip, mobile, message } = formData;
+    console.log('gs spreadsheet id:', process.env.GS_SPREADSHEET_ID);
+    console.log('client email:', env.GS_CLIENT_EMAIL);
+    console.log('private key:', process.env.GS_PRIVATE_KEY)
     // initialize the Google Sheet
     try {
       const doc = new GoogleSpreadsheet(process.env.GS_SPREADSHEET_ID);
+      console.log('doc:', doc);
       // initialize auth to Google Sheet
       await doc.useServiceAccountAuth({
         client_email: process.env.GS_CLIENT_EMAIL,
@@ -16,7 +20,7 @@ class InteractWithGoogleSheet {
       await doc.loadInfo();
       // set current sheet
       const sheet = doc.sheetsByIndex[0];
-
+      console.log('sheet:', sheet);
       // add row
       await sheet.addRow({
         firstName,
