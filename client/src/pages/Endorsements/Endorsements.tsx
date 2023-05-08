413e76d0fe703dff256c60a2a7b39a086ce5f5a5
asb1512 <andrew@olympicmedia.com>
2023-05-08 13:07:46 -0400

    edited copy on 'candidates'

--- client/src/pages/Endorsements/Endorsements.tsx
+++ client/src/pages/Endorsements/Endorsements.tsx
@@ -38,7 +38,7 @@ export default function Endorsements() {
 
       <div className={styles.wrapper}>
         <p>
-          Elect Common Sense is proud to stand behind these excellent candidates. We share a common vision for a more affordable New Jersey. A New Jersey that supports parents and children. A New Jersey that stands up to the radical left. A New Jersey that can take back our state once and for all.
+          Elect Common Sense is proud to stand behind these excellent candidates. We share a common vision for a more affordable New Jersey. A New Jersey that supports parents and children against the left's woke agenda. A New Jersey that stands up to the radical left. A New Jersey that can take back our state once and for all.
         </p>
 
         <div className={cardStyles.wrapper}>
