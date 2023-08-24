4daf6f50c7f2f03a75ef061b77d630ba657bf7a9
asb1512 <asb1512@gmail.com>
2023-08-24 15:28:07 -0400

    submit data to DS if mobile num presen

--- index.js
+++ index.js
@@ -18,13 +18,50 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-// frontend sends form data meant for Google Sheet
+// frontend sends form data here
+// data is sent to DirectSnd's endpoint
 app.post('/send', async (req, res) => {
-  await googleSheet.writeToSheet(req.body);
-  
-  res.status(400).json({
-    message: 'Bad request',
-  });
+  // always attempt to submit to Google Sheets
+  try {
+    const sheetSubmission = await googleSheet.writeToSheet(req.body);
+
+    // if mobile num present in req data
+    // submit to DS
+    if (req.body.mobile) {
+      try {
+        const headers = {
+          'Authorization': `token ${process.env.DS_AUTH_TOKEN}`,
+          'Content-Type': 'application/json',
+        };
+        const dsData = {
+          number: parseInt(req.body.mobile, 10),
+          first_name: req.body.firstName,
+          last_name: req.body.lastName,
+          email: req.body.email,
+          zip_code: parseInt(req.body.zip, 10),
+        };
+
+        fetch('https://app.directsnd.com/api/tagged_audience/', {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify(dsData),
+        });
+      }
+
+      catch (e) {
+        res.sendStatus(400).json({
+          message: e.message,
+        });
+      }
+    }
+    if (sheetSubmission) res.sendStatus(200);
+  }
+
+  catch {
+    res.sendStatus(400).json({
+      message: 'Bad request',
+    });
+  }
 });
 
 const port = process.env.PORT || 8000;
