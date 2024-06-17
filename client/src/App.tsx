1b45d2ab4df963ad455bd0822bd6b3a5b4a7fb90
asb1512 <andrew@olympicmedia.com>
2024-06-17 16:39:09 -0400

    hide site content and replace with only logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import FourZeroFour from './components/404/FourZeroFour';
-import Home from './pages/Home/Home';
-import Mission from './pages/Mission/Mission';
-import Advisors from './pages/Advisors/Advisors';
-import Contact from './pages/Contact/Contact';
-import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy';
-import Terms from './pages/Terms/Terms';
+// import { Routes, Route } from 'react-router-dom';
+// import { Toaster } from 'react-hot-toast';
+// import Navbar from './components/Navbar/Navbar';
+// import Footer from './components/Footer/Footer';
+// import FourZeroFour from './components/404/FourZeroFour';
+// import Home from './pages/Home/Home';
+// import Mission from './pages/Mission/Mission';
+// import Advisors from './pages/Advisors/Advisors';
+// import Contact from './pages/Contact/Contact';
+// import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy';
+// import Terms from './pages/Terms/Terms';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Toaster />
+      <div className="logo-wrapper">
+        <img src="/logo.png" alt="Elect Common Sense Logo" />
+      </div>
+      {/* <Toaster />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -26,7 +29,7 @@ function App() {
         <Route path="/terms-of-service" element={<Terms />} />
         <Route path="*" element={<FourZeroFour />} />
       </Routes>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
