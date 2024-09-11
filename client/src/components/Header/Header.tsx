f543ac8d018c920b556716b29fa87a7d5e8baae0
asb1512 <asb1512@gmail.com>
2024-09-11 18:09:54 -0400

    update props

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,19 +3,27 @@ import Stars from '../Stars/Stars';
 import styles from './Header.module.css';
 
 type Props = {
-  redText: string,
+  redText?: string,
   whiteText: string,
+  stars?: boolean;
   children: any,
 }
 
-export default function Header({ redText, whiteText, children }: Props) {
+export default function Header({
+  redText,
+  whiteText,
+  stars,
+  children,
+}: Props) {
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.heading}>
-        <span className={styles.red}>{redText}</span>&nbsp;
+        {redText && (
+          <span className={styles.red}>{redText}&nbsp;</span>
+        )}
         {whiteText}
       </h1>
-      <Stars />
+      {stars && <Stars />}
       {children}
     </div>
   );
